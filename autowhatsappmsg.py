63b7a618d7d669b0ee507b51b189f21343847928
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-05 21:43:13 +0530

    automatemsgwhatsapp

--- autowhatsappmsg.py
+++ autowhatsappmsg.py
@@ -1,3 +1,5 @@
 import pywhatkit
 pywhatkit.sendwhatmsg("+xxxxxxxxxx","sample",21,33)
-  
+"""we use this to send in specific time with hour in 24 format and minute as well
+and app and whatsapp has to be open on a browser so that it sends the msg or other
+ images by using the other methods as in place of 'pywhatkit.sendwhatmsg'."""
